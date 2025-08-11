7c3925f1803f4ba2377a3e739d7c1da0aa903a10
Janne <jabbekeipert@gmail.com>
2025-08-11 03:28:55 +0200

    test if linting fails

--- src/main/java/com/ase/userservice/controllers/RootController.java
+++ src/main/java/com/ase/userservice/controllers/RootController.java
@@ -9,6 +9,7 @@ public class RootController {
 
 	@GetMapping("/")
 	public ResponseEntity<String> root() {
+		String uselessVariable = "This is a useless variable";
 		return ResponseEntity.ok("API Root: /api/v1/");
 	}
 
