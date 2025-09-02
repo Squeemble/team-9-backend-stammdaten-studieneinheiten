e48acdb24326bcbaeae11823e6436841fe7141ef
ConConner <egal3d@gmail.com>
2025-09-02 13:41:24 +0200

    Cors policy for dev

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import api_router
 from app.core.db import Base, engine
 
@@ -9,3 +10,17 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="team-9-backend-service")
 app.include_router(api_router)
+
+# Configure CORS
+origins = [
+  "http://localhost:5173",  # Vite default
+  "http://127.0.0.1:5173",  # sometimes browser uses this
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],  # which origins are allowed
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
